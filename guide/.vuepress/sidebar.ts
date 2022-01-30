ae69e47f67617e822d03f930b5e8932b1eefe10f
GoneTone <p29022716@gmail.com>
2022-01-30 22:19:00 +0800

    Added permissions.md

--- guide/.vuepress/sidebar.ts
+++ guide/.vuepress/sidebar.ts
@@ -3,7 +3,8 @@ export default {
         {
             text: '首頁',
             children: [
-                '/'
+                '/',
+                '/permissions.md'
             ]
         },
         {
